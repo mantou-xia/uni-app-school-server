abe06818746afc222e62cb5a30eebe95d716b171
mantouxia <3775060926@qq.com>
2025-04-25 19:01:53 +0800

    gwb

--- unpackage/dist/dev/mp-weixin/common/vendor.js
+++ unpackage/dist/dev/mp-weixin/common/vendor.js
@@ -7290,9 +7290,9 @@ function initOnError() {
   };
 }
 function initRuntimeSocketService() {
-  const hosts = "192.168.197.1,192.168.226.1,10.0.118.17,127.0.0.1";
+  const hosts = "192.168.197.1,192.168.226.1,192.168.43.169,127.0.0.1";
   const port = "8090";
-  const id = "mp-weixin_aZS39I";
+  const id = "mp-weixin_TVcDEe";
   const lazy = typeof swan !== "undefined";
   let restoreError = lazy ? () => {
   } : initOnError();
